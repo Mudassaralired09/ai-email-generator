a504c1d24ea9c48b5b2ab6da2537dfdd59ea41c7
Mudassaralired09 <163699763+Mudassaralired09@users.noreply.github.com>
2025-06-25 17:14:30 +0500

    Update generateEmail.js

--- api/generateEmail.js
+++ api/generateEmail.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Only POST requests allowed' });
   }
